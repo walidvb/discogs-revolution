9e180aaf33fc9d37ac65644ad3ca2c945b1ae2ca
walidvb <walidvb@gmail.com>
2018-05-18 18:28:28 +0200

    handle blower errors

--- config/initializers/blower.rb
+++ config/initializers/blower.rb
@@ -2,6 +2,10 @@ module Blower
     def self.send_sms message
         url = ENV['BLOWERIO_URL']
         blowerio = RestClient::Resource.new(url)
-        blowerio['/messages'].post :to => '+33676952934‬', :message => message
+        begin
+            blowerio['/messages'].post :to => '+33676952934‬', :message => message
+        rescue => e
+            puts "Error posting sms #{e}"
+        end
     end
 end
